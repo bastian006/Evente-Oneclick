08c73433c503a8633324d0eb0706ffa906e75c8a
bastian006 <bastiannazy11@gmail.com>
2023-09-20 19:58:46 -0300

    Event-OneClick

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function bytext(){
+    document.getElementById("botonAdios").innerHTML = "Logout";
+}
+
+function ocultar(id){
+    var elemento = document.getElementById(id);
+    elemento.style.display = "none";
+}
+
+function alertBtn(id){
+    alert("ninja was liked!")
+}
